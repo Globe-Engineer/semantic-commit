878ce9ed788ea6d9c754b233e630927c6ff57360
Ivan Yevenko <iyevenko@gmail.com>
2023-10-08 20:25:41 -0400

    Update .gitignore and add LICENSE file

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
-    name='scommit',
+    name='semantic-commit',
     version='0.1',
+    description='scommit: ChatGPT-generated commit messages',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Globe-Knowledge-Solutions/semantic-commit',
+    author='Ivan Yevenko',
+    author_email='ivan@globe.engineer',
+    license='MIT',
     packages=find_packages(),
     install_requires=[
         'openai',
